c6989ca62f51a852fd1b9c52a78b050da6222127
Prince-Hervoet <2064128441@qq.com>
2023-06-07 09:22:23 +0000

    fix: #add codes

--- core/sstable.go
+++ core/sstable.go
@@ -2,10 +2,17 @@ package core
 
 import (
 	"DamperLSM/util"
+	"io"
+	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 )
+
+// 文件存储结构
+// 	    数据区			  索引区                 元数据区
+// valueLen value	offset keyLen key    dataAreaLen indexAreaLen
 
 // 磁盘映射结构
 type SstableController struct {
@@ -18,78 +25,111 @@ type SstableController struct {
 
 // 映射结构的头节点
 type SstableHeadNode struct {
-	next *SstableNode
+	head *SstableNode
 	size int32
 }
 
 // 映射结构的节点
 type SstableNode struct {
-	next *SstableNode
-	keys map[string]*FileIndexInfo
-}
-
-type FileIndexInfo struct {
-	valueLen int32
-	offset   int32
+	next     *SstableNode
+	fileName string
+	count    int32
+	keys     map[string]int32
 }
 
 func NewSstableController(dir string) *SstableController {
+	headList := make([]*SstableHeadNode, util.SSTABLE_LEVEL_SIZE)
+	for i := 0; i < len(headList); i++ {
+		headList[i] = &SstableHeadNode{
+			head: &SstableNode{
+				next:     nil,
+				fileName: "",
+				count:    -1,
+				keys:     nil,
+			},
+			size: 0,
+		}
+	}
 	return &SstableController{
-		headList: make([]*SstableHeadNode, util.SSTABLE_LEVEL_SIZE),
+		headList: headList,
 		maxLevel: 0,
 		nodeSize: 0,
 		dir:      dir,
 	}
 }
 
-func (here *SstableController) AddNodeFromFile(filePath string) {
+func (here *SstableController) SearchData(key string) (string, []byte, error) {
+	if here.nodeSize == 0 {
+		return "", nil, nil
+	}
+	headList := here.headList
+	for i := 0; i < len(headList); i++ {
+		shn := headList[i]
+		if shn.size == 0 {
+			continue
+		}
+		node := shn.head.next
+		for node != nil {
+			if _, has := node.keys[key]; has {
+				offset := node.keys[key]
+				filePath := here.dir + node.fileName
+				key, value, err := searchKvFromFile(filePath, offset)
+				if err != nil {
+					return "", nil, err
+				}
+				return key, value, nil
+			}
+			node = node.next
+		}
+	}
+	return "", nil, nil
+}
+
+func (here *SstableController) RecoverFromFiles() error {
 	// dbfile_1_2
-	// 获取第一层的最新一个id
-	// id := here.headList[0].size + 1
-	// 拼接文件名
-	// nFileName := util.DB_SAVE_FILE_NAME + "_" + "1" + "_" + strconv.FormatInt(int64(id), 10)
-	filePtr, err := os.OpenFile(filePath, os.O_RDWR, 0666)
+	dirInfo, err := ioutil.ReadDir(here.dir)
 	if err != nil {
-		return
+		return err
 	}
 
-	count, err := util.GetFileSize(filePath)
-	if err != nil {
-		return
-	}
-	if count < 8 {
-		return
-	}
+	for _, file := range dirInfo {
+		if file.IsDir() {
+			continue
+		}
+		fileName := file.Name()
+		if !strings.HasPrefix(fileName, util.DB_SAVE_FILE_NAME) {
+			continue
+		}
 
-	// 读取元数据区
-	indexLen, _, err := readMeta(filePtr)
+		ss := strings.Split(fileName, "_")
+		if len(ss) < 3 {
+			continue
+		}
 
-	indexBuffer := make([]byte, indexLen)
-	n, err := filePtr.Read(indexBuffer)
-	keys := make(map[string]*FileIndexInfo)
-	current := 0
-	for current < n {
-		fi := &FileIndexInfo{}
-		keyLen := util.BytesToInt32(indexBuffer[0:4])
-		valueLen := util.BytesToInt32(indexBuffer[4:8])
-		offset := util.BytesToInt32(indexBuffer[8:12])
-		key := string(indexBuffer[12 : 12+keyLen])
-		fi.offset = offset
-		fi.valueLen = valueLen
-		keys[key] = fi
-		current += int(12 + keyLen)
-	}
-	indexBuffer = nil
-	node := &SstableNode{
-		next: nil,
-		keys: keys,
-	}
-	here.mu.Lock()
-	defer here.mu.Unlock()
-	node.next = here.headList[0].next
-	here.headList[0].next = node
-	here.headList[0].size += 1
+		levelNumber, err := strconv.ParseInt(ss[1], 10, 64)
+		if err != nil {
+			continue
+		}
 
+		count, err := strconv.ParseInt(ss[2], 10, 64)
+		if err != nil {
+			continue
+		}
+
+		keys, err := getIndexDataFromFile(here.dir + fileName)
+		if err != nil {
+			continue
+		}
+
+		node := &SstableNode{
+			next:     nil,
+			fileName: fileName,
+			count:    int32(count),
+			keys:     keys,
+		}
+		here.addNode(int32(levelNumber), node)
+	}
+	return nil
 }
 
 func (here *SstableController) DumpMemory(immuTable *MemoryTable) {
@@ -105,30 +145,37 @@ func (here *SstableController) DumpMemory(immuTable *MemoryTable) {
 	target := immuTable.memoryStruct
 	run := target.headNode.Levels[0]
 	offsets := make([]int32, 0, target.size)
-	offsetRun := int32(0)
 	dataSizeRun := int32(0)
 	indexSizeRun := int32(0)
 
 	// 写入数据区
 	for run != nil {
-		key := []byte(run.Key)
 		value := run.Value
 		if value.Deleted {
 			run = run.Levels[0]
 			continue
 		}
-		filePtr.Write(key)
+		valueLenBs := util.Int32ToBytes(int32(len(value.Value)))
+		filePtr.Write(valueLenBs)
 		filePtr.Write(value.Value)
-		offsets = append(offsets, offsetRun)
-		dataSizeRun += (int32(len(key)) + int32(len(value.Value)))
+		offsets = append(offsets, dataSizeRun)
+		dataSizeRun += (4 + int32(len(value.Value)))
 		run = run.Levels[0]
 	}
 
 	// 写入索引区
-	for i := 0; i < len(offsets); i++ {
+	run = target.headNode.Levels[0]
+	i := 0
+	for run != nil {
+		keyBs := []byte(run.Key)
 		bs := util.Int32ToBytes(offsets[i])
+		keyLenBs := util.Int32ToBytes(int32(len(keyBs)))
 		filePtr.Write(bs)
-		indexSizeRun += int32(len(bs))
+		filePtr.Write(keyLenBs)
+		filePtr.Write(keyBs)
+		indexSizeRun += (4 + 4 + int32(len(keyBs)))
+		run = run.Levels[0]
+		i += 1
 	}
 
 	// 写入元数据区
@@ -136,6 +183,33 @@ func (here *SstableController) DumpMemory(immuTable *MemoryTable) {
 	dataLenBs := util.Int32ToBytes(dataSizeRun)
 	filePtr.Write(indexLenBs)
 	filePtr.Write(dataLenBs)
+}
+
+func (here *SstableController) addNode(level int32, node *SstableNode) {
+	headNode := here.headList[level]
+	run := headNode.head
+
+	if run.next == nil {
+		run.next = node
+		return
+	}
+
+	isOk := false
+	for run.next != nil {
+		if run.next.count < node.count {
+			temp := run.next
+			run.next = node
+			node.next = temp
+			isOk = true
+			break
+		} else {
+			run = run.next
+		}
+	}
+
+	if !isOk {
+		run.next = node
+	}
 }
 
 func readMeta(file *os.File) (int32, int32, error) {
@@ -147,4 +221,114 @@ func readMeta(file *os.File) (int32, int32, error) {
 	indexLen := util.BytesToInt32(buffer[0:4])
 	dataLen := util.BytesToInt32(buffer[4:])
 	return indexLen, dataLen, nil
+}
+
+func searchKvFromFile(filePath string, offset int32) (string, []byte, error) {
+	filePtr, err := os.OpenFile(filePath, os.O_RDWR, 0666)
+	if err != nil {
+		return "", nil, err
+	}
+	defer filePtr.Close()
+
+	// 设置偏移量
+	_, err = filePtr.Seek(int64(offset), 0)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// 数据区的格式 keyLen valueLen key value
+	//             4      4        ?   ?
+
+	lenBuffer := make([]byte, 4)
+
+	_, err = filePtr.Read(lenBuffer)
+	if err != nil {
+		return "", nil, err
+	}
+
+	keyLen := util.BytesToInt32(lenBuffer)
+
+	_, err = filePtr.Read(lenBuffer)
+	if err != nil {
+		return "", nil, err
+	}
+
+	valueLen := util.BytesToInt32(lenBuffer)
+
+	keyBuffer := make([]byte, keyLen)
+	valueBuffer := make([]byte, valueLen)
+
+	_, err = filePtr.Read(keyBuffer)
+	if err != nil {
+		return "", nil, err
+	}
+
+	_, err = filePtr.Read(valueBuffer)
+	if err != nil {
+		return "", nil, err
+	}
+
+	key := string(keyBuffer)
+	return key, valueBuffer, nil
+}
+
+func getIndexDataFromFile(filePath string) (map[string]int32, error) {
+	filePtr, err := os.OpenFile(filePath, os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer filePtr.Close()
+
+	keys := make(map[string]int32)
+	_, err = filePtr.Seek(0, io.SeekEnd)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = filePtr.Seek(-8, io.SeekCurrent)
+	if err != nil {
+		return nil, err
+	}
+
+	// 读取元数据区
+	lenBuffer := make([]byte, 4)
+	_, err = filePtr.Read(lenBuffer)
+	if err != nil {
+		return nil, err
+	}
+	dataAreaLen := util.BytesToInt32(lenBuffer)
+
+	_, err = filePtr.Read(lenBuffer)
+	if err != nil {
+		return nil, err
+	}
+	indexAreaLen := util.BytesToInt32(lenBuffer)
+
+	// 跳过数据区
+	_, err = filePtr.Seek(int64(dataAreaLen), io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+
+	// 开始读取索引区
+	buffer := make([]byte, indexAreaLen)
+	_, err = filePtr.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	current := int32(0)
+	for current < indexAreaLen {
+		offset := util.BytesToInt32(buffer[current : current+4])
+		keyLen := util.BytesToInt32(buffer[current+4 : current+8])
+		keyBuffer := make([]byte, keyLen)
+		_, err = filePtr.Read(keyBuffer)
+		if err != nil {
+			return nil, err
+		}
+		key := string(keyBuffer)
+		keys[key] = offset
+		current += (4 + 4 + keyLen)
+	}
+	return keys, nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"DamperLSM/core"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -11,9 +12,14 @@ func main() {
 		fmt.Println(err.Error())
 		return
 	}
+
+	start := time.Now().UnixMicro()
 	bs.Set("1", []byte("2jk"))
-	// bs.Set("3", []byte("2213"))
-	// bs.Set("4", []byte("2gfg"))
-	b, _ := bs.Get("3")
+	bs.Set("3", []byte("2213"))
+	bs.Set("4", []byte("2gfg"))
+	b, _ := bs.Get("1")
+	end := time.Now().UnixMicro()
+
+	fmt.Println((end - start))
 	fmt.Println(string(b))
 }
